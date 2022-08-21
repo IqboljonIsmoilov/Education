99feadd122d95bcce3102e0be8edcb41c5c96c2f
IQBOLJON <iqboljon.ismoilov.98mail.ru>
2022-08-21 14:23:00 +0500

    Education

---
+++ JavaScript/menu.js
@@ -0,0 +1,9 @@
+let nav = document.querySelector('.nav')
+
+        function showMenu () {
+            nav.style.right = '0'
+        }
+
+        function hideMenu () {
+            nav.style.right = '-200px'
+        }
